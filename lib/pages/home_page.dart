d78c66a4244cf842895743fcbecfd684d6a4148d
HappySolutions <heba.odesk.tasks@gmail.com>
2024-05-30 17:12:45 +0300

    adding pages

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,9 @@
+import 'package:final_project/pages/categories_page.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+
+import '../helpers/sql_helper.dart';
+import '../widgets/grid_view_widget.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -123,7 +128,7 @@ class _HomePageState extends State<HomePage> {
                       Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => CategoriesPage()));
+                              builder: (context) => const CategoriesPage()));
                     },
                   ),
                 ],
