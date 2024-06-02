411fcf51fb8074b66532b245604896e7a8f4b34c
HappySolutions <heba.odesk.tasks@gmail.com>
2024-06-02 13:04:32 +0300

    adding to Databse done

--- lib/pages/categories_ops_page.dart
+++ lib/pages/categories_ops_page.dart
@@ -1,5 +1,12 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:final_project/helpers/sql_helper.dart';
 import 'package:final_project/models/pos_category.dart';
+import 'package:final_project/widgets/app_eleveted_button.dart';
+import 'package:final_project/widgets/app_text_form_feild.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:sqflite/sqflite.dart';
 
 class CategoriesOpsPage extends StatefulWidget {
   final PosCategory? posCategory;
@@ -23,11 +30,77 @@ class _CategoriesOpsPageState extends State<CategoriesOpsPage> {
         padding: const EdgeInsets.all(20),
         child: Form(
           key: formKey,
-          child: const Column(
-            children: [],
+          child: Column(
+            children: [
+              AppTextFormFeild(
+                labelText: 'name',
+                controller: nameTextFeildController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please Enter valid name';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              AppTextFormFeild(
+                labelText: 'description',
+                controller: descriptionTextFeildController,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please Enter valid description';
+                  }
+                  return null;
+                },
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              AppElevatedButton(
+                label: widget.posCategory == null ? 'Submit' : 'Edit Category',
+                onPressed: () async {
+                  await onSubmit();
+                },
+              )
+            ],
           ),
         ),
       ),
     );
+  }
+
+  Future<void> onSubmit() async {
+    try {
+      var sqlHelper = GetIt.I
+          .get<SqlHelper>()
+          .db!
+          .insert('categories', conflictAlgorithm: ConflictAlgorithm.replace, {
+        'name': nameTextFeildController.text,
+        'description': descriptionTextFeildController.text,
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.green,
+          content: Text(
+            'Category Added Successfully',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      );
+      Navigator.pop(context);
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.red,
+          content: Text(
+            '==========> Error is $e',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      );
+    }
   }
 }
